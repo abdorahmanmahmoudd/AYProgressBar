b3485b102a6dd384c6055059a48215b107afdaea
Abdorahman <abdorahmanmahmoudd@gmail.com>
2018-12-22 15:50:41 +0200

    Initial Commit

---
+++ AYProgressBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AYProgressBar
+//
+//  Created by Abdorahman Youssef on 12/22/18.
+//  Copyright © 2018 Abdorahman Youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
